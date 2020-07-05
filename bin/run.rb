11a1bfe71b6e361bd836fd20ec886b44bef017e4
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-05 13:21:43 -0400

    errors fixed

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,3 @@
 require_relative '../config/environment'
 
-<<<<<<< HEAD
 puts "hello world"
-=======
-interactive_main = Main.new
-
-puts "hello world"
->>>>>>> a059cee739b634725999f35a3546170f364fcc79
