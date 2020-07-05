4670eceb09a6e840d4a97e7c1ec3024dd4f00590
gracetian2019 <gracetian4ever@gmail.com>
2020-07-05 17:12:17 -0400

    add some method

--- bin/run.rb
+++ bin/run.rb
@@ -3,12 +3,12 @@ require_relative '../config/environment'
 interface = Interface.new()
 interface.log_in_page
 
-# user_instance = interface.log_in_page
+user_instance = interface.log_in_page
 
-# unless user_instance
-#   user_instance = interface.log_in_page
-# end
+unless user_instance
+  user_instance = interface.log_in_page
+end
 
-# interface.user = user_instance
+interface.user = user_instance
 
-# interface.main_menu
+interface.main_menu
--- lib/app/models/task.rb
+++ lib/app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
     belongs_to :project
     belongs_to :user
+    def change_due_date(due_date)
+
+    end
 end
