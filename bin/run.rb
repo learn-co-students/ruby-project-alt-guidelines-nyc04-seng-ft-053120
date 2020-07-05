a044798ac55c601e3f7956fbd31050d82df4faaa
21shield <21netaly@gmail.com>
2020-07-05 13:23:39 -0400

    idk

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
 require_relative '../config/environment'
 
-interactive_main = Main.new
+# interactive_main = Main.new
 
 puts "hello world"
--- lib/apps/models/main.rb
+++ lib/apps/models/main.rb
@@ -1,7 +1,8 @@
 class Main
-    attr_accessor :prompt, :user
-    def initialize
-        @prompt = TTY::Prompt.new
-    end
+    # attr_accessor :prompt, :user
+
+    # def initialize
+    #     @prompt = TTY::Prompt.new
+    # end
     puts "Hello World Yo! HUHUHUHUHUH"
 end
