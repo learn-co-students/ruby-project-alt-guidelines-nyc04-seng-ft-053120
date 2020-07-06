ec7e5092224381df3cd293b831928af0b2deee15
vuonga1103 <vuonga1103@yahoo.com>
2020-07-06 09:40:51 -0400

    fixed some indentations

--- lib/app/models/task.rb
+++ lib/app/models/task.rb
@@ -1,10 +1,12 @@
 class Task < ActiveRecord::Base
     belongs_to :project
     belongs_to :user
-    # enum status: [:completed, :uncomplete]
-    def change_completed()
+    
+    def change_completed
       self.completed = true
+      self.save
     end
+
     def change_due_date(new_due_date)
      self.due_date = new_due_date
      self.save
--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -2,12 +2,13 @@ class User < ActiveRecord::Base
     has_many :collaborations
     has_many :projects, through: :collaborations
     has_many :tasks
-    attr_accessor :username, :collaborations, :projects, :tasks
+    
     def change_username(new_username)
         self.username = new_username
         self.save
     end
-    def create_new_project(name,description)
+
+    def create_new_project(name, description)
         new_project = Project.create(name: name, description: description)
         new_project.save
         new_collaboration = Collaboration.create(user: self, :project: new_project)
@@ -15,20 +16,23 @@ class User < ActiveRecord::Base
         new_ownership = Ownership.create(user: self, project: new_project)
         new_ownership.save
     end
-    def collaborate(project)
-       collaborate_project = Project.find_by(name: project.name)
-          if collaborate_project == nil 
-           puts" This project does not exist"
-          else 
-          Collaboration.create(user: self, project: project)  
-          end
-    end
-    def add_new_task_to_project(description, due_date, project)
-        found_project = Project.find(project.id)
-        if found_project
-        new_task = Task.create(description: description, completed: false, due_date: due_date, project: project, user: self)
-        end
-    end 
+
+    # def collaborate(project)
+    #    collaborate_project = Project.find_by(name: project.name)
+    #       if collaborate_project == nil 
+    #        puts" This project does not exist"
+    #       else 
+    #       Collaboration.create(user: self, project: project)  
+    #       end
+    # end
+
+    # def add_new_task_to_project(description, due_date, project)
+    #     found_project = Project.find(project.id)
+    #     if found_project
+    #     new_task = Task.create(description: description, completed: false, due_date: due_date, project: project, user: self)
+    #     end
+    # end 
+
     # def remove_self_from_project
     #    self.projects.find do |project| 
     #     if project_id == project.id
@@ -42,10 +46,11 @@ class User < ActiveRecord::Base
     #    end
     #    collaboration.destroy()
     # end
-    def projects_i_own
-      self.projects.select do |project|
-      if project.ownership.user_id == self.id
-        project
-      end
-    end
+
+    # def projects_i_own
+    #   self.projects.select do |project|
+    #   if project.ownership.user_id == self.id
+    #     project
+    #   end
+    # end
 end
