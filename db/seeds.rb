cd7b27f41598b56072796ea5168c654bbbd056ad
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-06 15:45:34 -0400

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,26 @@
 # Resource.destroy_all
 
 
-#user data User.create(username: ,password: ,name: ,age:  ,borough: ,user_id: )
-#should we use a username or user_id?
-user1 = User.create(username: "username_1", password: "username_1", name: "Sarah", age: 27, borough: "Brooklyn", user_id: 12)
+#user data User.create(username: ,password: ,name: ,age: ,borough: )
+user1 = User.create(username: "username_1", password: "username_1", name: "Sarah", age: 27, borough: "Brooklyn")
+user2 = User.create(username: "username_2", password: "username_2", name: "Sarah", age: 25, borough: "Bronx")
+user3 = User.create(username: "username_3", password: "username_3", name: "Sarah", age: 37, borough: "Manhattan")
+user4 = User.create(username: "username_4", password: "username_4", name: "Sarah", age: 49, borough: "Queens")
+user5 = User.create(username: "username_5", password: "username_5", name: "Sarah", age: 60, borough: "Staten Island")
+user6 = User.create(username: "username_6", password: "username_6", name: "Sarah", age: 52, borough: "Brooklyn")
+user7 = User.create(username: "username_7", password: "username_7", name: "Sarah", age: 48, borough: "Bronx")
+user8 = User.create(username: "username_8", password: "username_8", name: "Sarah", age: 73, borough: "Manhattan")
+user9 = User.create(username: "username_9", password: "username_9", name: "Sarah", age: 75, borough: "Queens")
+user10 = User.create(username: "username_10", password: "username_10", name: "Sarah", age: 55, borough: "Staten Island")
+
 #Faker::Name.name
 
-#userresource data Userresource.create(username: , user_id, resource_id)
-#for user_id, you could assign the attribute user_id = user.id OR user: name
-ur1 = Userresource.create(user_id: u1.id, resource: r1.id)
+# #userresource data Userresource.create(borough: ,practicioner:  ,user_id, resource_id)
+# #for user_id, you could assign the attribute user_id = user.id OR user: name
+# ur1 = Userresource.create(borough: , practicioner: , user_id: u1.id, resource: r1.id)
 
 
-#resource data Resource.create(resource_id, name:, location:, zipcode: ,url:)
-r1 = Resource.create(resource_id: 1, name: "myTherapyNYC" , location: " 928 Broadway, Suite 405/806, NYC 10010" , zipcode: "10010", url: "https://mytherapynyc.com/")
+# #resource data Resource.create(resource_id, name:, location:, zipcode: ,url:)
+# r1 = Resource.create(resource_id: 1, name: "myTherapyNYC" , location: "928 Broadway, Suite 405/806, NYC 10010" , zipcode: "10010", url: "https://mytherapynyc.com/")
 
 puts "done seeding!"
