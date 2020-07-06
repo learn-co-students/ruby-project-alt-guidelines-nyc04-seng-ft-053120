5804804516040481cf93b1eacf419361d4efc5a4
vuonga1103 <vuonga1103@yahoo.com>
2020-07-06 11:20:11 -0400

    fixed activerecord uninitialized bug

--- lib/app/models/task.rb
+++ lib/app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ActiveRecord::Base
     belongs_to :user
     
     def change_completed
-<<<<<<< HEAD
         self.completed = true
         self.save
     end
@@ -11,14 +10,5 @@ class Task < ActiveRecord::Base
     def change_due_date(new_due_date)
         self.due_date = new_due_date
         self.save
-=======
-      self.completed = true
-      self.save
-    end
-
-    def change_due_date(new_due_date)
-     self.due_date = new_due_date
-     self.save
->>>>>>> ec7e5092224381df3cd293b831928af0b2deee15
     end
 end
--- lib/app/models/user.rb
+++ lib/app/models/user.rb
@@ -1,57 +1,57 @@
 class User < ActiveRecord::Base
-    has_many :collaborations
-    has_many :projects, through: :collaborations
-    has_many :tasks
-    
-  def change_username(new_username)
-    self.username = new_username
-    self.save
-  end
+  has_many :collaborations
+  has_many :projects, through: :collaborations
+  has_many :tasks
+  
+def change_username(new_username)
+  self.username = new_username
+  self.save
+end
 
-  def create_new_project(name, description)
-    new_project = Project.create(name: name, description: description)
-    new_project.save
-    new_collaboration = Collaboration.create(user: self, project: new_project)
-    new_collaboration.save
-    new_ownership = Ownership.create(user: self, project: new_project)
-    new_ownership.save
-    new_project
-  end
+def create_new_project(name, description)
+  new_project = Project.create(name: name, description: description)
+  new_project.save
+  new_collaboration = Collaboration.create(user: self, project: new_project)
+  new_collaboration.save
+  new_ownership = Ownership.create(user: self, project: new_project)
+  new_ownership.save
+  new_project
+end
 
-  # def collaborate(project)
-    #    collaborate_project = Project.find_by(name: project.name)
-    #       if collaborate_project == nil 
-    #        puts" This project does not exist"
-    #       else 
-    #       Collaboration.create(user: self, project: project)  
-    #       end
-    # end
+# def collaborate(project)
+  #    collaborate_project = Project.find_by(name: project.name)
+  #       if collaborate_project == nil 
+  #        puts" This project does not exist"
+  #       else 
+  #       Collaboration.create(user: self, project: project)  
+  #       end
+  # end
 
-    # def add_new_task_to_project(description, due_date, project)
-    #     found_project = Project.find(project.id)
-    #     if found_project
-    #     new_task = Task.create(description: description, completed: false, due_date: due_date, project: project, user: self)
-    #     end
-    # end 
+  # def add_new_task_to_project(description, due_date, project)
+  #     found_project = Project.find(project.id)
+  #     if found_project
+  #     new_task = Task.create(description: description, completed: false, due_date: due_date, project: project, user: self)
+  #     end
+  # end 
 
-    # def remove_self_from_project
-    #    self.projects.find do |project| 
-    #     if project_id == project.id
-    #         project
-    #     end
-    #    end
-    #    project.collaborations.find do |collaboration|
-    #     if collaboration.user_id == self.id
-    #         collaboration
-    #     end
-    #    end
-    #    collaboration.destroy()
-    # end
+  # def remove_self_from_project
+  #    self.projects.find do |project| 
+  #     if project_id == project.id
+  #         project
+  #     end
+  #    end
+  #    project.collaborations.find do |collaboration|
+  #     if collaboration.user_id == self.id
+  #         collaboration
+  #     end
+  #    end
+  #    collaboration.destroy()
+  # end
 
-    # def projects_i_own
-    #   self.projects.select do |project|
-    #   if project.ownership.user_id == self.id
-    #     project
-    #   end
-    # end
+  # def projects_i_own
+  #   self.projects.select do |project|
+  #   if project.ownership.user_id == self.id
+  #     project
+  #   end
+  # end
 end
