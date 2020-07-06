ce28491190fce991dfed82194b555fd77cf2d466
vuonga1103 <vuonga1103@yahoo.com>
2020-07-06 10:44:25 -0400

    fixed some merge conflicts

--- bin/run.rb
+++ bin/run.rb
@@ -3,8 +3,4 @@ require_relative '../config/environment'
 interface = Interface.new()
 interface.log_in_or_register_page
 
-<<<<<<< HEAD
 user_instance = interface.log_in_or_register_page
-=======
-user_instance = interface.log_in_page
->>>>>>> ec7e5092224381df3cd293b831928af0b2deee15
