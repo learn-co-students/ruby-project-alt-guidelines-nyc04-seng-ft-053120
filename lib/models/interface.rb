617aee370a1733e9daa2b176faaee8f687551a60
kceron <karemiceron@gmail.com>
2020-07-07 12:56:55 -0400

    added reviews methods

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -39,4 +39,54 @@ class Interface
             menu.choice "Log out", -> {}
         end
     end
+
+    def main_menu
+        if user.name == nil 
+            return exit 
+        end
+        system "clear"
+        puts " "
+        puts "Welcome to Book Club #{user.name}!"
+        puts " "
+        answer = prompt.select("What would you like to do?") do |menu|
+            menu.choice "Search for reviews", -> { self.search_for_reviews }
+            menu.choice "Create a review", -> {}
+            menu.choice "Edit or delete a review", -> {}
+            menu.choice "Log out", -> { self.goodbye }
+        end
+    end
+
+    def search_for_reviews
+        system "clear"
+        puts " "
+        answer = prompt.ask("Enter the book title:")
+        book_instance = Book.all.find_by(title: answer)
+        if book_instance
+            self.get_book_reviews(book_instance)
+        else 
+            puts "That book has not been reviewed"
+        end 
+    end
+
+    def get_book_reviews(book_instance)
+        book_id = book_instance.id
+        #user_id = 
+        puts " "
+        puts "These are the reviews for:"
+        puts "\"#{book_instance.title}\""
+        puts "By: #{book_instance.author}"
+        puts " "
+
+        reviews_array = Review.all.select {|rev| rev.book_id == book_id }
+        reviews_array.each { |rev| 
+                    puts "Comment: #{rev.comment}"
+                    puts "Rating: #{rev.rating}"
+        }
+
+        #users_array = reviews_array.map {|rev| rev.user_id }
+    
+    end 
+
+
+
 end   
