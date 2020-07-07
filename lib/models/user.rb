ed7a152725b85240bf0f626c24a3cc8188578a7f
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-07 17:24:56 -0400

    returning_user

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
     has_many :resources, through: :userresources 
+
+
+    #self.create_new_user
+
+    
+    #self.returning_user
+    def self.returning_user
+    prompt = TTY::Prompt.new 
+    username_of_the_user = prompt.ask("Please enter your username:")
+    password_of_the_user = prompt.ask("Please enter your password:")
+    found_user = User.find_by(username: username_of_the_user, password: password_of_the_user)
+         if found_user
+             return found_user
+         else
+             puts " Sorry, that username and password combo is incorrect. Please reenter."
+        end
+    end
 end
 
 
