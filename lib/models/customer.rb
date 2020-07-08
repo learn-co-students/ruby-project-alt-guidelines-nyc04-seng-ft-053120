8d7e0984e2258473b4f61a1b46104ec33aa68f45
kelseyjdickson <kelseymedved@gmail.com>
2020-07-08 15:47:48 -0400

    adding code

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -1,66 +1,76 @@
-
-
 class Customer < ActiveRecord::Base
     has_many :orders
     has_many :items, through: :orders
-  
+    
+     #asking user for username, checks to see if it already exists and returns welcome, or returns sorry, name doesnt exist. 
     def self.get_instance(username)
-        get_instance = Customer.find_by(customer_name: username)
+       get_instance = Customer.find_by(customer_name: username)
         if get_instance
             sleep(1)
             puts "Welcome #{username}!"
-            get_instance
+             get_instance
         else
             sleep(1)
             puts "Sorry! That name doesn't exist!"
         end
     end
-  
+      
+     #Looking through Order class to return customer aka self
     def orders
-        Order.where(customer: self)
-      end
-   
+          Order.where(customer: self)
+    end
+      
+      #using method orders, to see how many orders the user has and then prints out the order instance and item name, 
+      #otherwise if not orders returns sorry, no orders
       def view_order
-        puts "Here are your orders!"
-        orders.each do |order|
-            puts "#{order} #{order.item.item_name}"
-          end
+        if orders.length > 0
+            puts "You have #{orders.length} order(s)!"
+        orders.each { |order| puts "#{order} #{ order.item.item_name }" }
+        else
+          puts "Sorry, you have no orders!"
         end
-   
-        def create_order
-          item_names = Item.all.map do |item|
-            {item.item_name => item}
-          end
-          item = TTY::Prompt.new.select("Choose an item", item_names)
-          Order.create(customer_id: self.id, item_id: item.id)
-          puts "Your order has been created for #{item.item_name}"
-        end
-   
-        def update_order
-          order_items= orders.map do |order|
-            {order.item.item_name => order}
-          end
-          order= TTY::Prompt.new.select("Choose an order", order_items)
-          new_items= Item.all.map do |item|
-            {item.item_name => item}
-          end
-          new_item= TTY::Prompt.new.select("Choose an item", new_items)
-          update_order=Order.find_by(id: order.id)
-          update_order.item=new_item
-          update_order.save
-          puts "Your #{order} has been updated!"
-        end
-   
-      def delete_order
-        order_items= orders.map do |order|
-          {order.item.item_name => order.id}
-        end
-        order_id= TTY::Prompt.new.select("Choose an order", order_items)
-        Order.destroy(order_id)
-        puts "Your order #{order_id} is deleted!"
       end
-   
-  
- end
- 
+      
+      #choose_order checks to see if we have an order, allows us to pick our items associated with our id, if we dont have any items, returns sorry, no orders
+    def choose_order
+        if orders.length > 0
+          order_items = orders.map { |order| { order.item.item_name => order.id } }
+          TTY::Prompt.new.select("Choose an order:", order_items)
+        else
+          puts "Sorry, you have no orders!"
+        end
+      end
 
+      #choose_item allows us to pick the item after choosing the order 
+      
+      def choose_item
+        item_names = Item.all.map { |item| { item.item_name => item } }
+        TTY::Prompt.new.select("Choose an item:", item_names)
+      end
+
+      #create_order takes the functionality from choose_item method and 
+      #allows us to take the item selected by the user and create it, associating it with chosen_item & customer(self)
+      def create_order
+          chosen_item = choose_item
+          create_order = Order.create(customer: self, item: chosen_item)
+          puts "Your order #{create_order} is created!"
+        end
+
+        #update_order uses methods (choose_order) which allows user to see list of orders and choose 
+        #them (choose_item) then updating that order with a new item, and saving while printing out to screen that the new item has been updated
+        def update_order
+            chosen_order_id = choose_order
+            chosen_item = choose_item
+            update_order = Order.find(chosen_order_id)
+            update_order.item = chosen_item
+            update_order.save
+            puts "Your order #{chosen_order_id} is updated!"
+        end
+      
+        #delete_order allows us to use the (choose_order) method and select the item we want to delete. Once Item is chosen, method returns the item that has been deleted
+        def delete_order
+          chosen_order_id = choose_order
+          Order.destroy(chosen_order_id)
+          puts "Your order #{chosen_order_id} is deleted!" 
+      end
+end
