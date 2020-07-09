0128beaff380854db389cc942e834731adba8a6e
kelseyjdickson <kelseymedved@gmail.com>
2020-07-09 10:00:12 -0400

    new code update

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -1,3 +1,4 @@
+
 class Customer < ActiveRecord::Base
     has_many :orders
     has_many :items, through: :orders
@@ -24,53 +25,73 @@ class Customer < ActiveRecord::Base
       #otherwise if not orders returns sorry, no orders
       def view_order
         if orders.length > 0
+            sleep(1)
             puts "You have #{orders.length} order(s)!"
+            sleep(1)
         orders.each { |order| puts "#{order} #{ order.item.item_name }" }
         else
+            sleep(1)
           puts "Sorry, you have no orders!"
         end
       end
       
+
+         #choose_item allows us to pick the item after choosing the order 
+      
+         def choose_item
+            item_names = Item.all.map { |item| { item.item_name => item } }
+            TTY::Prompt.new.select("Choose an item:", item_names)
+          end
+
+
       #choose_order checks to see if we have an order, allows us to pick our items associated with our id, if we dont have any items, returns sorry, no orders
     def choose_order
-        if orders.length > 0
-          order_items = orders.map { |order| { order.item.item_name => order.id } }
-          TTY::Prompt.new.select("Choose an order:", order_items)
-        else
-          puts "Sorry, you have no orders!"
-        end
-      end
+        order_item_names = orders.map { |order| {order.item.item_name => order} }
+        TTY::Prompt.new.select("Choose an order:", order_item_names)
+        
+    end
 
-      #choose_item allows us to pick the item after choosing the order 
-      
-      def choose_item
-        item_names = Item.all.map { |item| { item.item_name => item } }
-        TTY::Prompt.new.select("Choose an item:", item_names)
-      end
+   
 
       #create_order takes the functionality from choose_item method and 
       #allows us to take the item selected by the user and create it, associating it with chosen_item & customer(self)
       def create_order
           chosen_item = choose_item
           create_order = Order.create(customer: self, item: chosen_item)
-          puts "Your order #{create_order} is created!"
+          sleep(1)
+          puts "-------------------------------------------------"
+          puts "Your order #{create_order} is created for #{chosen_item.item_name}!"
+          puts "-------------------------------------------------"
         end
 
         #update_order uses methods (choose_order) which allows user to see list of orders and choose 
         #them (choose_item) then updating that order with a new item, and saving while printing out to screen that the new item has been updated
         def update_order
-            chosen_order_id = choose_order
+            if orders.length > 0
+            chosen_order = choose_order
             chosen_item = choose_item
-            update_order = Order.find(chosen_order_id)
+            update_order = Order.find(chosen_order.id)
             update_order.item = chosen_item
             update_order.save
-            puts "Your order #{chosen_order_id} is updated!"
+            puts "Your order #{chosen_order} is updated with #{chosen_item.item_name}!"
+            else 
+                sleep(1)
+                puts "Sorry, you have no orders!"
+            end
         end
       
         #delete_order allows us to use the (choose_order) method and select the item we want to delete. Once Item is chosen, method returns the item that has been deleted
         def delete_order
-          chosen_order_id = choose_order
-          Order.destroy(chosen_order_id)
-          puts "Your order #{chosen_order_id} is deleted!" 
-      end
+            if orders.length > 0
+
+          chosen_order = choose_order
+          Order.destroy(chosen_order.id)
+          sleep(1)
+
+          puts "Your order #{chosen_order} for #{chosen_order.item.item_name} is deleted!" 
+            else 
+                sleep(1)
+                puts "Sorry, you have no orders!"
+            end
+        end
 end
--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -3,6 +3,8 @@ class Interface
      def initialize
       @prompt = TTY::Prompt.new
     end
+
+    #displays welcome message 
      def welcome
         puts"               Welcome to"
         puts"                         ,--.          ,--."
@@ -11,15 +13,21 @@ class Interface
         puts"|  |  |  |\ '-'  ||  |   |  \ \\   --. |  |"
         puts"`--`--`--' `--`--'`--'   `--'`--'`----'`--'"
     end
+
+    #get user input
+    def user_input 
+        sleep(1)
+        puts "Please enter your Markel username:"
+        gets.chomp
+    end
      
-    
+    #get customer instance 
     def get_instance
-      sleep(1)
-      puts "Please enter your Markel username:"
-      username = gets.chomp
+      username = user_input
       Customer.get_instance(username)
     end
   
+    #show the menu 
     def show_menu
       prompt.select("What would you like to do?") do |menu|
         menu.choice "Create Order", -> {self.create_order}
@@ -30,32 +38,40 @@ class Interface
     end
   end
   
+  #Displaying customer's orders
   def view_order
     customer.view_order
-    sleep(3)
+    sleep(2)
     show_menu
   end
-  
+
+
+  #Allows customer to create an order 
   def create_order
     customer.create_order
-    sleep(3)
+    sleep(2)
     show_menu
   end
   
+  #Customer updates an order 
   def update_order
     customer.update_order
-    sleep(3)
+    sleep(2)
     show_menu
   end
   
+  #customer deletes an order 
   def delete_order
     customer.delete_order
-    sleep(3)
+    sleep(2)
     show_menu
   end
   
+  #Customer able to exit app
    def exit
-    puts "Thanks for visiting #{customer.customer_name}!"
+
+    sleep(1)
+    puts "Bye #{customer.customer_name}! Thanks for visiting!"
   end
   
  end
