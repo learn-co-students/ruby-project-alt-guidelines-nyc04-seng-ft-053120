06dd53d1902c95e6438ec19707fa10ea7c9f8742
Soxinmypocket <shawnaejohnson6@gmail.com>
2020-07-09 12:13:23 -0400

    12:13

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Resource.destroy_all
 #User (username: password: name: age: borough: )
 #Resource (name: practitioner: borough: url: )
 #Userresource (user_id: resource_id: borough: practitioner: )
+
 user1 = User.create(username: "Molly", password: "Dolly", name: "Holly", age: 27, borough: "Brooklyn")
 user2 = User.create(username: "Shelly", password: "Smelly", name: "Belly", age: 25, borough: "Bronx")
 user3 = User.create(username: "Ty", password: "Fly", name: "Blye", age: 37, borough: "Manhattan")
@@ -14,53 +15,54 @@ userresource_2 = Userresource.create(user_id: 124, resource_id: 456, borough: "M
 userresource_3 = Userresource.create(user_id: 125, resource_id: 789, borough: "Staten Island", practitioner: "She" )
 
 
-resource_1 = Resource.create(name: "Bob", practitioner: "Dog", borough: "Brooklyn", url:"google.com" )
-resource_2 = Resource.create(name: "San", practitioner: "Man", borough: "Brooklyn", url: "google.com" )
-resource_3 = Resource.create(name: "Glee", practitioner: "She", borough: "Manhattan", url:"google.com" )
+# resource_1 = Resource.create(name: "Bob", practitioner: "Dog", borough: "Brooklyn", url:"google.com" )
+# resource_2 = Resource.create(name: "San", practitioner: "Man", borough: "Brooklyn", url: "google.com" )
+# resource_3 = Resource.create(name: "Glee", practitioner: "She", borough: "Manhattan", url:"google.com" )
 
 
 
 
-# 3.times do
-#     Resource.create(
-#         name: Faker::Name.name,
-#         practitioner: "Psychiatrist",
-#         location: Faker::Address.street_address,
-#         borough: "Brooklyn",
-#         url: Faker::Internet.url
-#     )
-#     Resource.create(
-#         name: Faker::Name.name,
-#         practitioner: "Psychologist",
-#         location: Faker::Address.street_address,
-#         borough: "Brooklyn",
-#         url: Faker::Internet.url
-#     )
-#     Resource.create(
-#         name: Faker::Name.name,
-#         practitioner: "Counselor",
-#         location: Faker::Address.street_address,
-#         borough: "Brooklyn",
-#         url: Faker::Internet.url
-#     )
-#     Userresource.create(
-#         user_id: rand(1..10),
-#         resource_id: rand(1..10),
-#         borough: "Staten Island",
-#         practitioner: "Psychiatrist"
-#     )
-#     Userresource.create(
-#         user_id: rand(1..10),
-#         resource_id: rand(1..10),
-#         borough: "Staten Island",
-#         practitioner: "Psychologist"
-#     )
-#     Userresource.create(
-#         user_id: rand(1..10),
-#         resource_id: rand(1..10),
-#         borough: "Staten Island",
-#         practitioner: "Counselor"
-#     )
-# end
+3.times do
+    Resource.create(
+        name: Faker::Name.name,
+        practitioner: "Psychiatrist",
+        location: Faker::Address.street_address,
+        borough: "Brooklyn",
+        url: Faker::Internet.url
+    )
+    Resource.create(
+        name: Faker::Name.name,
+        practitioner: "Psychologist",
+        location: Faker::Address.street_address,
+        borough: "Brooklyn",
+        url: Faker::Internet.url
+    )
+    Resource.create(
+        name: Faker::Name.name,
+        practitioner: "Counselor",
+        location: Faker::Address.street_address,
+        borough: "Brooklyn",
+        url: Faker::Internet.url
+    )
+    Userresource.create(
+        user_id: rand(1..10),
+        resource_id: rand(1..10),
+        borough: "Staten Island",
+        practitioner: "Psychiatrist"
+    )
+    Userresource.create(
+        user_id: rand(1..10),
+        resource_id: rand(1..10),
+        borough: "Staten Island",
+        practitioner: "Psychologist"
+    )
+    Userresource.create(
+        user_id: rand(1..10),
+        resource_id: rand(1..10),
+        borough: "Staten Island",
+        practitioner: "Counselor"
+    )
+end
+
  puts "done seeding :bug:" 
 
--- lib/models/resource.rb
+++ lib/models/resource.rb
@@ -30,122 +30,25 @@ class Resource < ActiveRecord::Base
         arg = Resource.all.select {|resource| resource[:borough] == borough_name}
         puts "Here are the available practitioners in #{borough_name}"
         provider_names = arg.map do |provider|     
-            {"#{provider[:name]} | #{provider[:practitioner]} | #{provider[:url]}" => provider.id}    
+            {"#{provider[:name]} | #{provider[:practitioner]} | #{provider[:borough]} | #{provider[:url]}" => provider.id}
+            #add to a separate method    
         end
 
-        if !provider_names.empty?     
+       # if !provider_names.empty?     
             provider_id = TTY::Prompt.new.select('Select a provider', provider_names)      
             found_provider = Resource.all.find_by_id(provider_id)      
                 found_provider   
-        else
-            puts 'No providers are available in your area'      
-            sleep(4)    
-        end
+        # else
+        #     puts 'No providers are available in your area'      
+        #     sleep(4)    
+        #end
     end
 
-    def self.create_record(borough_name, provider)
-        record = Userresource.create(user_id: self.id, resource_id: provider.id, borough: provider[:borough], practitioner: provider[:practitioner] )
+    def self.create_record(provider)
+        #record = self.create(user_id: self.id, resource_id: provider.id, borough: provider[:borough], practitioner: provider[:practitioner] )
+        new_ur = Userresource.create(user_id: self.id, resource_id: provider.id, borough:  provider[:borough], practitioner: provider[:practitioner])
         puts "Record created"
         record 
     end
-
 end
 
-
-# #     def self.bronx
-# #         system "clear"
-# #             puts ""
-# #             arg = Resource.where(borough: "Bronx")
-# #             puts "Here are the available practicioners in Bronx"
-# #             arg.map do |doc|
-# #                 puts doc.name
-# #                 puts ""
-# #             end
-# #         end
-# #     end
-
-# #     def self.manhattan
-# #         system "clear"
-# #         puts ""
-# #         arg = Resource.where(borough: "Manhattan")
-# #             puts "Here are the available practicioners in Manhattan"
-# #         arg.map do |doc|
-# #             puts doc.name
-# #             puts ""
-# #         end
-# #     end
-
-# #     def self.queens
-# #         system "clear"
-# #         puts ""
-# #         arg = Resource.where(borough: "Queens")
-# #         puts "Here are the available practicioners in Queens"
-# #         arg.map do |doc|
-# #             puts doc.name
-# #             puts ""
-# #         end
-# #     end
-
-# #     def self.statenisland
-# #         system "clear"
-# #         puts ""
-# #         arg = Resource.where(borough: "Staten Island")
-# #         puts "Here are the available practicioners in Staten Island"
-# #         arg.map do |doc|
-# #             puts doc.name
-# #             puts ""
-# #         end
-# #     end
-# # end
-        
-    
-        
-#         #bklyn
-#         # bklyn = Resource.find_by(borough: "Brooklyn")
-#         # return bklyn
-#     #         return found_borough
-#     #     # else
-#     #     #     puts "There are no practicioners in your Borough. Please try another Borough."
-        
-     
-    
-
-
-
-
-
-
-
-
-                
-#     # #borough practicioners display
-#     # def self.find_by_borough(brooklyn)
-#     #     brooklyn_practicioners = self.all.select do |practicioner|
-#     #     practicioner.borough == "Brooklyn"
-#     #     end
-#     #     system "clear"
-#     #     puts " Here are your available practicioners in brooklyn:"
-#     #     puts""
-#     #     brooklyn_practicioners.map do |p|
-#     #         puts p.name
-#     #         puts""
-#     #     end
-#     # end
-# #system "clear"
-    
-
-#     # def self.find_by_borough(b)
-#     #    # system "clear"
-#     #     puts "Borough Database Search"
-#     #     #go into the database and select all of the practicioners from that borough selected 
-#     #     x = self.all.select do |practicioner|
-#     #         practicioner.b == borough
-#     #         puts "search done"
-#     #     end
-
-
-#         # TTY::Prompt.new.select("Please select a Practicioner to search by:") do |menu|
-#         #     menu.choice "Psychologists"#, -> {self.search_by_practicioner}
-#         #     menu.choice "Psychiatrist"#, -> {self.search_by_practicioner}
-#         #     menu.choice "Social "#, -> {self.search_by_practicioner}
-#         #     menu.choice "Return to the Main Menu", -> {Interface.mai
